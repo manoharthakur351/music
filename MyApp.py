66672f749173d88d5e889a020de93d904114dc84
Manohar Thakur Oppo <tsmile2459@gmail.com>
2021-03-09 01:27:54 +0530

    ....

--- MyApp.py
+++ MyApp.py
@@ -13,11 +13,14 @@ from kivy.uix.image import AsyncImage
 from kivy.uix.scrollview  import ScrollView
 from kivy.uix.screenmanager import Screen,ScreenManager,NoTransition, FadeTransition, RiseInTransition
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import OneLineAvatarListItem , TwoLineAvatarListItem, ThreeLineAvatarListItem, OneLineIconListItem, TwoLineIconListItem ,ThreeLineIconListItem
+from kivymd.uix.list import OneLineAvatarListItem
 from kivymd.uix.toolbar import MDToolbar
-from kivymd.uix.list import MDList , OneLineAvatarListItem
+from kivymd.uix.list import MDList
 from kivymd.uix.navigationdrawer import MDNavigationDrawer, NavigationLayout
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelThreeLine
 from kivymd.app import MDApp
 
 	
@@ -37,7 +40,7 @@ def load ():
 	Builder.load_file("screens/screen1.kv")
 	Builder.load_file("screens/screen2.kv")
 	Builder.load_file("screens/screen3.kv")
-	Builder.load_file("screens/screen4.kv")
+	Builder.load_file("screens/sub1/abt.kv")
 	load_sub1()
 	
 	
@@ -80,24 +83,30 @@ class MaiApp (MDApp):
 			
 	def textit (self,path):
 		"""
-		this function is to load and display text
+		this function is to load
+		and display text
 		from different files
 		"""
 		file = open(path)
 		txt = file.read()
 		return txt
 
-	def show_alert_dialog(self):
+	def collab(self):
+		'''
+		function to display
+		dialogue box
+		'''
 		if not self.dialog:
 			self.dialog = MDDialog(
-				text="Discard draft?",
+				title="Collaborators",
+				text='•Abhishek\n•Aditya\n•Kundan\n•Manohar',
 				buttons=[
 					MDFlatButton(
-						text="CANCEL", text_color=self.theme_cls.primary_color
+						text="", text_color=self.theme_cls.primary_color
 					),
 					MDFlatButton(
-						text="DISCARD", text_color=self.theme_cls.primary_color
-					),
+						text="OK", text_color=self.theme_cls.primary_color
+					)
 				],
 			)
 		self.dialog.open()
