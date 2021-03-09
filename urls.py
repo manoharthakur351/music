151f455954718fbc427df8aed91edd087dfdbb64
Manohar Thakur Oppo <tsmile2459@gmail.com>
2021-03-09 11:07:23 +0530

    added insta links

--- urls.py
+++ urls.py
@@ -4,5 +4,9 @@ pythonorg = "https://python.org"
 kivy = "https://kivy.org"
 
 class we :
+	sheku_i='https://instagram.com/hurcanspyder?igshid=1i46zmuqyiw8p'
+	adi_i='https://instagram.com/adiittya._.02?igshid=1d0gy0spvea2y'
+	me_i=adi_i
 	instagram = "https://www.instagram.com/manoharlal_thakur/"
 	github = "https://github.com/manoharthakur351"
+	source='https://github.com/manoharthakur351/music'
